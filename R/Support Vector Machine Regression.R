f7fde7d64a056a4b8e76752cbed0787dea9a0769
Ferian Rian <rian_ferian@apps.ipb.ac.id>
2021-07-20 18:54:34 +0700

    SVM

---
+++ R/Support Vector Machine Regression.R
@@ -0,0 +1,32 @@
+# Load The dataset
+data("iris")
+dataset <- iris
+
+# We only need the numeric data
+# Asume Petal Width was the Y and the other is X
+# Remove Species
+dataset <- dataset[-5]
+
+# Splitting the dataset into the Training set and Test set
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Petal.Width, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+
+# Fitting Multiple Linear Regression to the Training set
+library(e1071)
+regressor = svm(formula = Petal.Width ~ .,
+                data = dataset,
+                type = 'eps-regression',
+                kernel = 'radial')
+
+# Predicting the Test set results
+y_pred = predict(regressor, newdata = test_set)
+
+# MSE (Mean Squared Error)
+sum((test_set$Petal.Width - y_pred)^2)
+
+# MAPE (Mean Absolute precentage error)
+sum(abs(test_set$Petal.Width - y_pred))
